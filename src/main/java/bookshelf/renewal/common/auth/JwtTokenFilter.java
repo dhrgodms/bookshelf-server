522bc680be562048e08a2d5d0715b6c0ca71e334
dhrgodms <dhrgodms123@gmail.com>
2025-07-06 21:03:37 +0900

    feat: jwtTokenFilter 추가

--- src/main/java/bookshelf/renewal/common/auth/JwtTokenFilter.java
+++ src/main/java/bookshelf/renewal/common/auth/JwtTokenFilter.java
@@ -1,15 +1,68 @@
 package bookshelf.renewal.common.auth;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JwtTokenFilter extends GenericFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        String token = httpServletRequest.getHeader("Authorization");
+        System.out.println("token = " + token);
+
+        try {
+            if (token != null) {
+                if (!token.substring(0, 7).equals("Bearer ")) {
+                    throw new AuthenticationServiceException("Not Bearer Form");
+                }
+                String jwtToken = token.substring(7);
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJwt(jwtToken)
+                        .getBody();
+
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
+
+                UserDetails userDetails = new User(claims.getSubject(), "", authorities);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, jwtToken, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+            httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.getWriter().write("Invalid Token");
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
