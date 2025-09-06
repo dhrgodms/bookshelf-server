ccb839128bdff2c5ef5de3a5956b609f33ac58ac
dhrgodms <dhrgodms123@gmail.com>
2025-09-06 15:11:44 +0900

    deploy: github actions deploy.yml

--- src/test/java/bookshelf/renewal/BookshelfRenewalApplicationTests.java
+++ src/test/java/bookshelf/renewal/BookshelfRenewalApplicationTests.java
@@ -1,13 +1,8 @@
 package bookshelf.renewal;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookshelfRenewalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
